cc05f505c9d226d2c2dfc2125dd0da06608b71bd
DLMcCoy88 <48062956+DLMcCoy88@users.noreply.github.com>
2021-11-19 09:47:20 -0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0d3c5e71.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
